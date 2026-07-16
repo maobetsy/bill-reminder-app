7d31c8fb8369fb5e22ed84be2ad62f398b4bf5ff
Betsy <maobetsy@gmail.com>
2026-07-16 16:25:48 +1000

    setting up state and props for BillList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import Header from '@/components/Header';
-import BillForm from '@/components/BillForm';
-import BillList from '@/components/BillList';
+"use client";
+
+import { useState } from "react";
+
+import Header from "@/components/Header";
+import BillForm from "@/components/BillForm";
+import BillList from "@/components/BillList";
 
 export default function Home() {
+  const [bills, setBills] = useState([]); 
+
   return (
     <main>
       <Header />
       <BillForm />
-      <BillList />
+      <BillList bills={bills}/>
     </main>
   );
 }
